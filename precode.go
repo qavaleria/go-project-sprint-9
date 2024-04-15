cb7e713cb58ac634baa21f44ecba7a67e65ee4f0
qavaleria <qavaleria.spain@gmail.com>
2024-04-15 14:49:15 +0200

    first iteration

--- precode.go
+++ precode.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -13,20 +15,37 @@ import (
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 	// 1. Функция Generator
-	// ...
+	defer close(ch)
+	for i := int64(1); ; i++ {
+		select {
+		case <-ctx.Done():
+			return
+		case ch <- i:
+			fn(i)
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
-	// ...
+	defer close(out)
+	for i := range in {
+		out <- i
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
-
+	var wgCtx sync.WaitGroup
+	wgCtx.Add(1)
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	context.AfterFunc(ctx, func() {
+		wgCtx.Done()
+		return
+	})
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -34,8 +53,8 @@ func main() {
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		atomic.AddInt64(&inputSum, i)
+		atomic.AddInt64(&inputCount, 1)
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -55,7 +74,21 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
+	for i, out := range outs {
+		wg.Add(1)
+		go func(ch <-chan int64, i int) {
+			defer wg.Done()
+			for {
+				num, ok := <-ch
+				if !ok {
+					return
+				}
+				atomic.AddInt64(&amounts[i], 1)
+				chOut <- num
+			}
+
+		}(out, i)
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -68,13 +101,28 @@ func main() {
 	var sum int64   // сумма чисел результирующего канала
 
 	// 5. Читаем числа из результирующего канала
-	// ...
+	go func(ch <-chan int64) {
+		wgCtx.Add(1)
+		defer wgCtx.Done()
+		for {
+			num, ok := <-ch
+			if !ok {
+				return
+			}
+			atomic.AddInt64(&count, 1)
+			atomic.AddInt64(&sum, num)
+		}
+
+	}(chOut)
+
+	wgCtx.Wait()
 
 	fmt.Println("Количество чисел", inputCount, count)
 	fmt.Println("Сумма чисел", inputSum, sum)
 	fmt.Println("Разбивка по каналам", amounts)
 
 	// проверка результатов
+	fmt.Println(inputSum, sum)
 	if inputSum != sum {
 		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum, sum)
 	}
